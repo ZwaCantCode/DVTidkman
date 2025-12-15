ea0f7ac31465d223cef7501dca636cc76fd24fc9
Jo <116927170+ZwaCantCode@users.noreply.github.com>
2025-12-15 15:49:04 +0700

    add key

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const chatbotToggler = document.querySelector("#chatbot-toggler");
 const closeChatbot = document.querySelector("#close-chatbot");
 
 // API setup
-const API_KEY = "#####";
+const API_KEY = "AIzaSyBb46qKf9GPtuxNg9iduirffpdwe_ALvYQ";
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 
 const userData = {
